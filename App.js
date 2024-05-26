61cb3de4b3e4a70fcc4196d78101cf8caf697a7d
yashk6 <150025548+yashk6@users.noreply.github.com>
2024-05-26 13:22:36 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+import logo from "../../assets/react-core-concepts.png";
+import "./Header.css";
+
+const reactDescriptions = ["Fundamental", "crucials", "core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={logo} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
